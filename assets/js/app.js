71330baeed9ff6c3fdb3ec020de48aaf29a1f460
Sheina Cunningham <cunninghamsheina@gmail.com>
2021-03-23 00:28:37 -0400

    fixed folders

---
+++ assets/js/app.js
@@ -0,0 +1,7 @@
+document.write('hello world');
+console.log('hello')
+const x = 'Hello World';
+document.getElementById('idOne').innerHTML = x;
+
+var myValue = document.getElementById('idOne').textContent;
+console.log()
